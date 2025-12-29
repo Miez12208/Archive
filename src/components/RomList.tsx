d354debce71cac764f7b546159d9bcaf61e135a6
Miez12208 <ngopisantuy76@gmail.com>
2025-12-29 12:45:09 +0800

    feat: add RomList component to display ROM data.

--- src/components/RomList.tsx
+++ src/components/RomList.tsx
@@ -73,7 +73,7 @@ const ROM_DATA = [
 
 export default function RomList() {
     return (
-        <section id="rom-list" className="pb-20 md:pb-32 px-4 md:px-6 bg-black">
+        <section id="rom-list" className="pt-20 md:pt-32 pb-20 md:pb-32 px-4 md:px-6 bg-black">
             <div className="max-w-7xl mx-auto">
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8">
                     {ROM_DATA.map((rom, idx) => (
